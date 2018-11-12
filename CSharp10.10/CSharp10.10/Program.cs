1cf8e81da97d74412a247d57741763c90492bce7
Vadim231 <40603603+Vadim231@users.noreply.github.com>
2018-11-12 21:57:58 +0300

    Add files via upload

---
+++ CSharp10.10/CSharp10.10/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp10._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Tank[] winner = new Tank[5];
+                Tank[] t34 = new Tank[5];
+                Tank[] pantera = new Tank[5];
+                for (int i = 0; i < t34.Length; i++)
+                {
+                    winner[i] = new Tank();
+                }
+
+                //заполнение
+                Random random = new Random();
+                for (int i = 0; i < t34.Length; i++)
+                {
+                    t34[i] = new Tank("T34", random.Next(0, 100), random.Next(0, 100), random.Next(0, 100));
+                }
+                for (int i = 0; i < t34.Length; i++)
+                {
+                    pantera[i] = new Tank("Pantera", random.Next(0, 100), random.Next(0, 100), random.Next(0, 100));
+                }
+
+                for (int i = 0; i < 5; i++)
+                {
+                    t34[i].Print();
+                    pantera[i].Print();
+                    winner[i] = t34[i] * pantera[i];
+                    Console.WriteLine("В " + (i + 1) + " схватке победил: ");
+                    winner[i].Print();
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Clear();
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+
+
+        
+        }
+    }
+}
---
+++ CSharp10.10/CSharp10.10/Tank.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp10._10
+{
+    class Tank
+    {
+        protected string name;
+        protected int armorLevel;
+        protected int ammunition;
+        protected int maneuverability;
+
+        public Tank()
+        {
+            name = "";
+            armorLevel = 0;
+            ammunition = 0;
+            maneuverability = 0;
+        }
+
+        public Tank(string name,int ammunition, int armorLevel, int maneuverability)
+        {
+
+            this.name = name;
+            this.armorLevel = armorLevel;
+            this.ammunition = ammunition;
+            this.maneuverability = maneuverability;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("name: " + name);
+            Console.WriteLine("ammunition: " + ammunition + ", armorLevel: " + armorLevel + ", maneuverability: " + maneuverability);
+        }
+
+        public static Tank operator *(Tank firstTank, Tank secondTank)
+        {
+
+            if ((firstTank.ammunition > secondTank.ammunition && firstTank.armorLevel > secondTank.armorLevel)
+                ||(firstTank.ammunition > secondTank.ammunition && firstTank.maneuverability > secondTank.maneuverability)
+                    ||(firstTank.armorLevel > secondTank.armorLevel && firstTank.maneuverability > secondTank.maneuverability))
+            {
+                return firstTank;
+            }
+            else if((firstTank.ammunition == secondTank.ammunition && firstTank.armorLevel == secondTank.armorLevel)
+                || (firstTank.ammunition == secondTank.ammunition && firstTank.maneuverability == secondTank.maneuverability)
+                    || (firstTank.armorLevel == secondTank.armorLevel && firstTank.maneuverability == secondTank.maneuverability))
+            {
+                Console.WriteLine("Танки подружились и уехали в закат");//вероятность очень мала
+                return secondTank;
+            }
+            else
+            {
+                return secondTank;
+            }
+        }
+    }
+}
